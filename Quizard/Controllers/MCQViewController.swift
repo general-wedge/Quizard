bbf1040c6abe669162f19ae4f35a4aa186e068dd
Evan Kysley <evan@kysley.com>
2018-11-27 14:30:51 -0500

    Scaffold MCQ

--- Quizard/Controllers/MCQViewController.swift
+++ Quizard/Controllers/MCQViewController.swift
@@ -10,12 +10,50 @@ import UIKit
 
 class MCQViewController: UIViewController {
 
+    var questionIndex: Int = 0
+
+    @IBOutlet weak var labelQuestionIndex: UILabel!
+    @IBOutlet weak var labelQuestionBody: UILabel!
+    @IBOutlet weak var buttonOption1: UIButton!
+    @IBOutlet weak var buttonOption2: UIButton!
+    @IBOutlet weak var buttonOption3: UIButton!
+    @IBOutlet weak var buttonOption4: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+
+    @IBAction func submitChoice(sender: AnyObject) {
+        //check if what we chose was correct
+        guard let button = sender as? UIButton else {
+            return
+        }
+        switch button.tag {
+        case 1:
+            verifyAnswer(answer: 1)
+        case 2:
+            verifyAnswer(answer: 2)
+        case 3:
+            verifyAnswer(answer: 3)
+        case 4:
+            verifyAnswer(answer: 4)
+        default:
+            print("unknown answer")
+            return
+        }
+    }
+
+    func verifyAnswer(answer: Int) {
+        // firebase call or logic check against the selectedQuestionIndex
+        print(answer)
+    }
+
+    func readyView() {
+        // step 1: get data from the correct index
+        // step 2: update ui pieces accordingly
+    }
 
     /*
     // MARK: - Navigation
